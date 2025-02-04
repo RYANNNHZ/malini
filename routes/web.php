81772054ed6bc931ec70f5e45bde7616817ac382
RYANNNHZ <riantojelek871@gmail.com>
2025-02-04 10:20:06 +0700

    benerin rute

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/halamanlogin');
 });
 
 Route::get('/admin',[AdminController::class,'admin'])->middleware('isAdmin');
